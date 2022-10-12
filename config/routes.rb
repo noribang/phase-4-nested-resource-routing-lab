c2e64eb0fc3545403b4e609785d96247f6f1a49f
hoonkim <hoonk9387@gmail.com>
2022-10-12 10:58:20 -0500

    Add comments to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+
+  # items      GET  /items(.:format)                    items#index
+  # user_items GET  /users/:user_id/items(.:format)     items#index
+  #            POST /users/:user_id/items(.:format)     items#create
+  #  user_item GET  /users/:user_id/items/:id(.:format) items#show
+  #       user GET  /users/:id(.:format)                users#show
+  # resources :items, only: [:index]
   resources :users, only: [:show] do
     resources :items, only: [:index, :show, :create]
   end
